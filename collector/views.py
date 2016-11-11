28d4f585f0a65009e756c26d0181e43821124ea5
mazucci <may.zurb@gmail.com>
2016-11-11 13:04:33 +0100

    corrects concat

--- collector/views.py
+++ collector/views.py
@@ -313,9 +313,9 @@ def json_OL_heatmap(request, days=2, platform='FSQ', date_start=None, date_end=N
             if d[i] != 0:
                 ps.append({'lat': lat_centres[i], 'lng': lon_centres[i], 'count': d[i]})
         jfile['data'] = ps
-        cache.set('jfile_%_%_%' % (platform, date_start, date_end), jfile)
+        cache.set('jfile_%s_%s_%s' % (platform, date_start, date_end), jfile)
     else:
-        jfile = cache.get('jfile_%_%_%' % (platform, date_start, date_end), jfile)
+        jfile = cache.get('jfile_%s_%s_%s' % (platform, date_start, date_end), jfile)
     
     return JsonResponse()
 
